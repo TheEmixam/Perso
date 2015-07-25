c4cd017103e6ed69c75a6c7c85bdc3074cd4d0c9
TheEmixam <maxxixxgsm@gmail.com>
2015-07-25 16:32:59 +0200

    geometry

--- SDL_install/SDL_install/base.cpp
+++ SDL_install/SDL_install/base.cpp
@@ -70,12 +70,12 @@ bool LoadMedia()
 	bool success = true;
 
 	//Load PNG texture
-	g_pTexture = loadTexture("img/texture.png");
-	if (g_pTexture == nullptr)
-	{
-		printf("Failed to load texture!\n");
-		success = false;
-	}
+	//g_pTexture = loadTexture("img/texture.png");
+	//if (g_pTexture == nullptr)
+	//{
+	//	printf("Failed to load texture!\n");
+	//	success = false;
+	//}
 
 	return success;
 }
@@ -119,10 +119,32 @@ void Update()
 	//SDL_BlitSurface(g_pStretchedSurface, NULL, g_pScreenSurface, NULL);
 
 	//Clear screen
+	SDL_SetRenderDrawColor(g_pRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 	SDL_RenderClear(g_pRenderer);
 
 	//Render texture to screen
-	SDL_RenderCopy(g_pRenderer, g_pTexture, NULL, NULL);
+	//SDL_RenderCopy(g_pRenderer, g_pTexture, NULL, NULL);
+
+	//Render red filled quad
+	SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
+	SDL_SetRenderDrawColor(g_pRenderer, 0xFF, 0x00, 0x00, 0xFF);
+	SDL_RenderFillRect(g_pRenderer, &fillRect);
+
+	//Render green outlined quad
+	SDL_Rect outlineRect = { SCREEN_WIDTH / 6, SCREEN_HEIGHT / 6, SCREEN_WIDTH * 2 / 3, SCREEN_HEIGHT * 2 / 3 };
+	SDL_SetRenderDrawColor(g_pRenderer, 0x00, 0xFF, 0x00, 0xFF);
+	SDL_RenderDrawRect(g_pRenderer, &outlineRect);
+
+	//Draw blue horizontal line
+	SDL_SetRenderDrawColor(g_pRenderer, 0x00, 0x00, 0xFF, 0xFF);
+	SDL_RenderDrawLine(g_pRenderer, 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2);
+
+	//Draw vertical line of yellow dots
+	SDL_SetRenderDrawColor(g_pRenderer, 0xFF, 0xFF, 0x00, 0xFF);
+	for (int i = 0; i < SCREEN_HEIGHT; i += 4)
+	{
+		SDL_RenderDrawPoint(g_pRenderer, SCREEN_WIDTH / 2, i);
+	}
 
 	//Update screen
 	SDL_RenderPresent(g_pRenderer);
